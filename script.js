d63ba74b931a3fa26f1a3892094c3b921da758f2
aklemm03 <aklemm_a@mail.ru>
2025-03-20 19:11:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const mediaQuerys = window.matchMedia("(max-width:700px)")
+if (mediaQuerys.matches === true) {
+mySwiper = new Swiper(".swiper", {
+  spaceBetween:16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: "auto",
+}
+)
+} else {
+}
